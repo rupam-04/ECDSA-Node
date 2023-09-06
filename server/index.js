b413e8f6caad2de3ad3b25100047142bc9c3603e
rupam-04 <rpmdey2004@gmail.com>
2023-09-06 21:40:09 +0530

    completed

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3042;
+const secp = require("ethereum-cryptography/secp256k1");
+const {keccak256} = require("ethereum-cryptography/keccak");
 
 app.use(cors());
 app.use(express.json());
@@ -19,7 +21,20 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, sig:sigStringed, msg } = req.body;
+  const { recipient, amount } = msg;
+
+  const sig = {
+    ...sigStringed,
+    r: BigInt(sigStringed.r),
+    s: BigInt(sigStringed.s),
+  }
+  
+  const hashMessage = (message) => keccak256(Uint8Array.from(message));
+  const isValid = secp.secp256k1.verify(sig, hashMessage(message), publicKey) === true;
+  if (!isValid) {
+    res.status(400).send({ message: "Invalid signature!" });
+  }
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
@@ -32,6 +47,8 @@ app.post("/send", (req, res) => {
     res.send({ balance: balances[sender] });
   }
 });
+
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
